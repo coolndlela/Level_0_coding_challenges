18b9a28e39bf343d9fb2039478f43afebeb201d3
coolndlela <sabelon8@gmail.com>
2021-12-13 15:15:57 +0200

    Adding the 5th task into the repo

---
+++ Task_05.py
@@ -0,0 +1,8 @@
+# Task 0.5
+from math import sqrt
+
+
+def calc_triangle_area(num1, num2, num3):
+    semi_perimeter = (num1 + num2 + num3) / 2
+    area = sqrt(semi_perimeter*((semi_perimeter-num1)*(semi_perimeter-num2)*(semi_perimeter-num3)))
+    return area
