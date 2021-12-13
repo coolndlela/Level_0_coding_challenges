e9e654bcfc8239a6e7fef4594dc9a1137cdf5667
coolndlela <sabelon8@gmail.com>
2021-12-13 18:18:26 +0200

    Adding task 7 into the repo

---
+++ Task_07.py
@@ -0,0 +1,17 @@
+# Task 0.7
+# converts Celsius to Fahrenheit
+def celsius_to_fahrenheit(celsius):
+    fahrenheit = (celsius * 1.8) + 32
+    return fahrenheit
+
+
+print(celsius_to_fahrenheit(20))
+
+
+# Converts Fahrenheit to Celsius
+def fahrenheit_to_celsius(fahrenheit):
+    celsius = (fahrenheit - 32) * 5 / 9
+    print(celsius)
+
+
+fahrenheit_to_celsius(68)
