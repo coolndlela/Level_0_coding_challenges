93c8fe3c534693f8a8161fa7117b38b75a184952
coolndlela <sabelon8@gmail.com>
2021-12-13 20:39:57 +0200

    Fixng spelling mistakes

--- Task_05.py
+++ Task_05.py
@@ -6,3 +6,4 @@ def calc_triangle_area(num1, num2, num3):
     semi_perimeter = (num1 + num2 + num3) / 2
     area = sqrt(semi_perimeter*((semi_perimeter-num1)*(semi_perimeter-num2)*(semi_perimeter-num3)))
     return area
+
