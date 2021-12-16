4c6e38c7ab1104869f3cea3d478129f18b47f566
coolndlela <sabelon8@gmail.com>
2021-12-16 04:09:28 +0200

    Adding task 8 to repo

---
+++ Task_08.py
@@ -0,0 +1,20 @@
+# Task 0.8
+
+def to_hrs_and_minutes(num):
+    hours = num//60
+    minutes = num % 60
+
+    if hours > 1 and minutes > 1:
+        print (f"{hours} hours, {minutes} minutes")
+
+    elif minutes <= 1 and hours <= 1:
+        print(f"{hours} hour, {minutes} minute")
+
+    elif hours > 1 and minutes <= 1:
+        print (f"{hours} hours, {minutes} minute")
+
+    else:
+        print (f"{hours} hour, {minutes} minutes")
+
+
+to_hrs_and_minutes(3333)
