a1bd3663912a9b7690207d2ce655dbd3aab5667a
coolndlela <sabelon8@gmail.com>
2021-12-17 18:53:09 +0200

    initial commit

---
+++ Task_09.py
@@ -0,0 +1,22 @@
+# Task 0.9
+from __future__ import print_function
+
+
+def find_vowels():
+
+    string = input("Enter word ").lower()
+    vowels = "aeiou"
+    new_string = ""     # stores the vowels for the string
+    for vowl in string:
+        if vowl in vowels:
+            new_string = new_string + vowl
+
+    # from the new string of vowels, the below loop eliminates duplicates if any
+    output = ""
+    for char in new_string:
+        if char not in output:
+            output = output + char
+            print(char, end=",")
+
+
+find_vowels()
