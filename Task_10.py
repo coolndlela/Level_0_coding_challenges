a302d2df657c1c0f502e72e0479e3c24acfee8a6
coolndlela <sabelon8@gmail.com>
2021-12-20 08:43:46 +0200

    Initial commit

---
+++ Task_10.py
@@ -0,0 +1,17 @@
+#Task 0.10
+
+def common_char(string1, string2):
+
+    common = ""
+    for char in string1:
+        if char in string2:
+            common = common + char
+
+    output = ""
+    for i in common:
+        if i not in output:
+            output = output + i
+    print(*output, sep=",")
+
+
+common_char("computers", "house")
