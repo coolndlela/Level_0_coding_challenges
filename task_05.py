3adfe48fe343b9f58b13f29e1148f3c3afdbcb1e
coolndlela <sabelon8@gmail.com>
2022-01-10 16:25:42 +0200

    removed the built-in funtion from my function

--- task_05.py
+++ task_05.py
@@ -1,8 +1,9 @@
 # Task 0.5
-from math import sqrt
-
 
 def calc_triangle_area(num1, num2, num3):
     semi_perimeter = (num1 + num2 + num3) / 2
-    area = sqrt(semi_perimeter*((semi_perimeter-num1)*(semi_perimeter-num2)*(semi_perimeter-num3)))
+    area = (semi_perimeter*((semi_perimeter-num1)*(semi_perimeter-num2)*(semi_perimeter-num3))) ** 0.5
     return area
+
+
+calc_triangle_area(21, 10, 13)
