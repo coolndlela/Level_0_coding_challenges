f464d49cb0122de30c2f500167c8209a31c87774
coolndlela <sabelon8@gmail.com>
2022-01-10 15:01:19 +0200

    removed needless comments, and made both functions to return rather than print

--- task_07.py
+++ task_07.py
@@ -1,17 +1,16 @@
 # Task 0.7
-# converts Celsius to Fahrenheit
+
 def celsius_to_fahrenheit(celsius):
     fahrenheit = (celsius * 1.8) + 32
     return fahrenheit
 
 
-print(celsius_to_fahrenheit(33))
+celsius_to_fahrenheit(33)
 
 
-# Converts Fahrenheit to Celsius
 def fahrenheit_to_celsius(fahrenheit):
     celsius = (fahrenheit - 32) * 5 / 9
-    print(celsius)
+    return celsius
 
 
 fahrenheit_to_celsius(91.4)
