feaac30c882b08b62f04502196bee96c6bab9d07
coolndlela <sabelon8@gmail.com>
2022-01-10 15:16:39 +0200

    Modified the code so that 0 minutes, and 0 hours is plural

--- task_08.py
+++ task_08.py
@@ -4,17 +4,17 @@ def to_hrs_and_minutes(num):
     hours = num//60
     minutes = num % 60
 
-    if hours > 1 and minutes > 1:
+    if hours > 1 and minutes > 1 or hours == 0 and minutes == 0:
         print (f"{hours} hours, {minutes} minutes")
 
     elif minutes <= 1 and hours <= 1:
         print(f"{hours} hour, {minutes} minute")
 
-    elif hours > 1 and minutes <= 1:
+    elif hours > 1 and minutes == 1:
         print (f"{hours} hours, {minutes} minute")
 
     else:
         print (f"{hours} hour, {minutes} minutes")
 
 
-to_hrs_and_minutes(3333)
+to_hrs_and_minutes(0)
