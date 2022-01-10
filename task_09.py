2331848882b1f598b7be3cf7100253cc8173817b
coolndlela <sabelon8@gmail.com>
2022-01-10 14:50:53 +0200

    Removed needless comments

--- task_09.py
+++ task_09.py
@@ -4,12 +4,11 @@ def find_vowels():
 
     string = input("Enter word ").lower()
     vowels = "aeiou"
-    new_string = ""     # stores the vowels for the string
+    new_string = ""
     for vowl in string:
         if vowl in vowels:
             new_string = new_string + vowl
 
-    # from the new string of vowels, the below loop eliminates duplicates if any
     output = ""
     for char in new_string:
         if char not in output:
