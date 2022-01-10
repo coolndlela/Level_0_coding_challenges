89ea81809c82cc6b7671a3d01354cec86aaba053
coolndlela <sabelon8@gmail.com>
2022-01-10 16:54:06 +0200

    upgraded the first function instead of writing a new one

--- task_06.py
+++ task_06.py
@@ -1,17 +1,18 @@
 # Task 0.6
 def get_max_num(num1, num2, num3):
-    if num1 >= num2 and num1 >= num3:
-        return num1
-    elif num2 >= num1 and num2 >= num3:
-        return num2
-    else:
-        return num3
+    numbers = (num1, num2, num3)
+    max_num = numbers[0]
+
+    for i in numbers:
+        if i > max_num:
+            max_num = i
+
+    return max_num
 
 
-print(get_max_num(1, 4, 12))
+get_max_num(66, 4, 12)
 
 
-# upgrading the above function any amount of numbers
 def get_max_num(*numbers):
     max_num = numbers[0]
     for i in numbers:
@@ -21,4 +22,4 @@ def get_max_num(*numbers):
     return max_num
 
 
-print(get_max_num(1, 22, 3, 2))
+get_max_num(1, 22, 3, 2)
