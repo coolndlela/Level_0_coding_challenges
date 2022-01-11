e8acfbccbdfcde99908e7dda8c3c2c829042c02a
coolndlela <sabelon8@gmail.com>
2022-01-11 08:53:22 +0200

    replaced everything with one function

--- task_06.py
+++ task_06.py
@@ -1,17 +1,4 @@
 # Task 0.6
-def get_max_num(num1, num2, num3):
-    numbers = (num1, num2, num3)
-    max_num = numbers[0]
-
-    for i in numbers:
-        if i > max_num:
-            max_num = i
-
-    return max_num
-
-
-get_max_num(66, 4, 12)
-
 
 def get_max_num(*numbers):
     max_num = numbers[0]
@@ -22,4 +9,4 @@ def get_max_num(*numbers):
     return max_num
 
 
-get_max_num(1, 22, 3, 2)
+print(get_max_num(1, 3, 2, 4, 10))
